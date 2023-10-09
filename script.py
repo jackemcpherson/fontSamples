89f2a47edc256378c23c431214e3e5ef26bf5be1
Jack McPherson <jmcpherson@mauriceblackburn.com.au>
2023-10-09 11:40:01 +1100

    Updated script with new driver code.

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import os
-from main import generate_font_sample
+from main import FontSampleGenerator
 
 # Define constant parameters
 text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -7,9 +7,16 @@ image_size = (250, 250)
 font_size = 35
 directory = "./fonts/"  # replace with the path to your directory
 
+# Ensure the output directory exists
+if not os.path.exists("./output_files/"):
+    os.makedirs("./output_files/")
+
 # Loop through all .ttf files in the directory
 for filename in os.listdir(directory):
     if filename.endswith(".ttf"):
         font_path = os.path.join(directory, filename)
         output_path = f"./output_files/{filename[:-4]}.png"  # remove the .ttf extension and add .png
-        generate_font_sample(font_path, text, image_size, font_size, output_path)
+
+        # Initialize FontSampleGenerator and generate the sample
+        generator = FontSampleGenerator(font_path, text, image_size, font_size)
+        generator.generate_sample(output_path)
