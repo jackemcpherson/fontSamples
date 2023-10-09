0665d055b53f703bcd93133f7843cd83427ffea7
Jack McPherson <jmcpherson@mauriceblackburn.com.au>
2023-10-09 11:12:02 +1100

    refactor to class based approach.

--- main.py
+++ main.py
@@ -1,59 +1,65 @@
-import textwrap
+from typing import Tuple
 from PIL import Image, ImageFont, ImageDraw
+import textwrap
 
 
-def generate_font_sample(font_path, text, image_size, font_size, output_path):
-    # Create a new image with white background
-    img = Image.new("RGB", image_size, "#F8F5F0")
-    draw = ImageDraw.Draw(img)
+class FontSampleGenerator:
+    def __init__(
+        self, font_path: str, text: str, image_size: Tuple[int, int], font_size: int
+    ):
+        self.font_path = font_path
+        self.text = text
+        self.image_size = image_size
+        self.font_size = font_size
 
-    # Load the font
-    try:
-        font = ImageFont.truetype(font_path, font_size)
-    except IOError:
-        print(f"Unable to load font at {font_path}")
-        return
+    def _wrap_text(self, font: ImageFont.FreeTypeFont, max_width: int) -> list:
+        """Wrap text to fit within a specified width."""
+        bbox = font.getbbox("A")
+        return textwrap.wrap(self.text, width=int(max_width / (bbox[2] - bbox[0])))
 
-    # Calculate the maximum width of a line
-    max_width = image_size[0] - 20  # subtract a small margin
+    def generate_sample(self, output_path: str) -> None:
+        """Generate font sample and save to the given output path."""
+        img = Image.new("RGB", self.image_size, "#F8F5F0")
+        draw = ImageDraw.Draw(img)
 
-    # Wrap the text
-    lines = textwrap.wrap(text, width=int(max_width / font_size))
+        try:
+            font = ImageFont.truetype(self.font_path, self.font_size)
+        except IOError as e:
+            raise Exception(f"Unable to load font at {self.font_path}. Error: {e}")
 
-    # Calculate total height of the text
-    total_text_height = len(lines) * font.getsize(lines[0])[1]
+        max_width = self.image_size[0] - 20
+        lines = self._wrap_text(font, max_width)
 
-    # Calculate start height to center vertically
-    start_height = (image_size[1] - total_text_height) / 2
+        # Adjust font size if the first line is too wide
+        while (
+            draw.textbbox((0, 0), lines[0], font=font)[2] > max_width
+            and self.font_size > 10
+        ):
+            self.font_size -= 1
+            font = ImageFont.truetype(self.font_path, self.font_size)
+            lines = self._wrap_text(font, max_width)
 
-    current_height = start_height  # start drawing text from the start height
-    for line in lines:
-        text_width, text_height = draw.textsize(line, font=font)
-        if (
-            text_width > max_width
-        ):  # if the line is still too wide, reduce the font size
-            while text_width > max_width:
-                font_size -= 1
-                font = ImageFont.truetype(font_path, font_size)
-                text_width, text_height = draw.textsize(line, font=font)
-
-        # Calculate the width of the line to center it
-        width = (image_size[0] - text_width) / 2
-        # Draw the line
-        draw.text((width, current_height), line, fill="black", font=font)
-
-        current_height += (
-            text_height  # update the current height position for the next line
+        total_text_height = len(lines) * (
+            font.getbbox(lines[0])[3] - font.getbbox(lines[0])[1]
         )
+        start_height = (self.image_size[1] - total_text_height) / 2
 
-    # Save the image
-    img.save(output_path)
+        current_height = start_height
+        for line in lines:
+            text_width = draw.textbbox((0, 0), line, font=font)[2]
+            text_height = draw.textbbox((0, 0), line, font=font)[3]
+            width = (self.image_size[0] - text_width) / 2
+            draw.text((width, current_height), line, fill="black", font=font)
+            current_height += text_height
+
+        img.save(output_path)
 
 
-font_path = "./fonts/reader-medium-pro.ttf"  # replace with the path to your .ttf file
-text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-image_size = (250, 250)  # replace with the desired image size
-font_size = 45  # replace with the desired font size
-output_path = "output_files/output.png"  # replace with the desired output path
-
-generate_font_sample(font_path, text, image_size, font_size, output_path)
+if __name__ == "__main__":
+    generator = FontSampleGenerator(
+        font_path="./fonts/reader-medium-pro.ttf",
+        text="ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        image_size=(250, 250),
+        font_size=45,
+    )
+    generator.generate_sample(output_path="output_files/output.png")
